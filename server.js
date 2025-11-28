b43f2c793a7ae5820bbb82840736732a6b9c400e
Rykardh0 <hssricardo@gmail.com>
2025-11-28 18:50:56 -0300

    Alteracoes_28.11

--- server.js
+++ server.js
@@ -1,38 +1,50 @@
-import { Prisma, PrismaClient } from '@prisma/client'
 import express from 'express'
-
-//const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-//const usuarios = []
+const usuarios = [  {
+    nome: "Ricardo",
+    matricula: "2510138",
+    nomeDaMae: "Segredo",
+    Sexo: 1,
+    Turno: 3
+  },
+  {
+    nome: "Tiruleibe",
+    matricula: "7777999",
+    nomeDaMae: "Tralaleiro",
+    Sexo: 1,
+    Turno: 2
+  }]
 
-app.post('/alunos', async (req, res)=> {
-    //console.log(req.body)
-    const novoAluno = await Prisma.user.create({
-        data: {
-            nome:   req.body.nome,
-            matricula:  req.body.matricula,
-            nomeDaMae:  req.body.nomeDaMae,
-            sexo:   req.body.sexo,     
-            turno:  req.body.turno        
-        }
-    })
-    //usuarios.push(req.body)
+app.post('/alunos',(req, res)=> {
+    console.log(req.body)
+    usuarios.push(req.body)
     res.status(201).send(req.body)
 })
 
-app.get('/alunos', async (req, res)=> {
-    const alunos = await Prisma.pAlunos.findMany()
-    res.status(200).json(alunos)
+app.get('/alunos',(req, res)=> {
+    res.status(200).json(usuarios)
 })
+
+app.delete('/alunos', (req,res)=> {
+    const deleteMatricula = req.body.matricula
+    const usuariod = usuarios.findIndex(aluno => aluno.matricula==deleteMatricula)
+    res.status(200).json(usuarios[usuariod].nome)
+    usuarios.splice(InputDeviceInfo, usuariod)
+
+})
+    
+
+
+
 
 app.listen(3000)
 
 
 
 /*
-app.delete('/alunos')
 app.put('/alunos')
+
 */
